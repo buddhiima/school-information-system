880e8667e862af7b2fc4a5e291faa54ce8e5378c
Buddhima Hewavitharana <buddhimahewa@gmail.com>
2022-09-09 20:29:42 +0530

    requirement change

---
+++ schoolinformationsystem/schoolinformationsystem/src/main/java/springfield/academy/schoolinformationsystem/controller/StudentController.java
@@ -0,0 +1,30 @@
+package springfield.academy.schoolinformationsystem.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import springfield.academy.schoolinformationsystem.modal.Student;
+import springfield.academy.schoolinformationsystem.service.StudentService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/home")
+@CrossOrigin
+public class StudentController {
+
+    private final StudentService studentService;
+
+    // constructor
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    // get all the students
+    @GetMapping("")
+    public ResponseEntity<List<Student>> getAllStudents() {
+        List<Student> students = studentService.getAllStudents();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+}
---
+++ schoolinformationsystem/schoolinformationsystem/src/main/java/springfield/academy/schoolinformationsystem/service/StudentService.java
@@ -0,0 +1,26 @@
+package springfield.academy.schoolinformationsystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import springfield.academy.schoolinformationsystem.modal.Student;
+import springfield.academy.schoolinformationsystem.repository.StudentRepository;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    // constructor
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    // get all students
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
+
