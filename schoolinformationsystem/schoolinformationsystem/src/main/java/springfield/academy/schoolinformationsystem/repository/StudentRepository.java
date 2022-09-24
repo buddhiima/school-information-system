a357b6b0c1ba623218b9d54247d51f823b732f94
raushan_rashid <72301703+Raushan2001@users.noreply.github.com>
2022-09-24 16:30:06 +0530

    Changes 1

---
+++ schoolinformationsystem/schoolinformationsystem/src/main/java/springfield/academy/schoolinformationsystem/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package springfield.academy.schoolinformationsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import springfield.academy.schoolinformationsystem.modal.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
